534fe00b43dd05862377088f099e509254e5fa50
jenielr97 <jenielr97@gmail.com>
2024-02-20 07:33:42 -0400

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,7 +3,6 @@
 * main - A program that print a line using the printf f
  untion
 * Return: Always 0 (Success)
-
 */
 int main(void)
 { puts("\with proper grammar, but the outcome is a piece of art,\n");
