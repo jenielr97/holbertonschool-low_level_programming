29ab171ad62130d52f3cab17e776648a4579fb81
jenielr97 <jenielr97@gmail.com>
2024-02-20 07:31:23 -0400

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
+ * main - A program that print a line using the printf f
+ untion
  * Return: Always 0 (Success)
+
  */
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");
+{ puts("\with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
