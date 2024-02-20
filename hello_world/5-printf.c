e24f149915eee80afb522f5f73ae2fa9868f3b10
jenielr97 <jenielr97@gmail.com>
2024-02-20 09:06:32 -0400

    printf script

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ puts("\with proper grammar, but the outcome is a piece of art,\n");
+{puts("\with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
