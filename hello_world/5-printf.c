0d3570308eafdc022af528dc516642114cae0a4d
jenielr97 <jenielr97@gmail.com>
2024-02-20 07:32:39 -0400

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main - A program that print a line using the printf f
+* main - A program that print a line using the printf f
  untion
- * Return: Always 0 (Success)
+* Return: Always 0 (Success)
 
- */
+*/
 int main(void)
 { puts("\with proper grammar, but the outcome is a piece of art,\n");
 return (0);
