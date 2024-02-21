203113d34d42c3465ae5b701bcd2bcd60e1b2f8b
jenielr97 <jenielr97@gmail.com>
2024-02-21 09:21:37 -0400

    8

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+  int i = '0';
