e81c80ae44518fcddec1b66160d1da919f62d0ec
jenielr97 <jenielr97@gmail.com>
2024-02-21 22:14:08 -0400

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * print_alphabet - this is the function of the task
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+char num, letter;
+
+for (num =0; num <= 9; num++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+}
