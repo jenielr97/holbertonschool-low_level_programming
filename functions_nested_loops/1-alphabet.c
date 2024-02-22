9d4aaf9cd5118a45af2144c430796884dfd9b616
jenielr97 <jenielr97@gmail.com>
2024-02-22 09:17:17 -0400

    lower

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * print_alphabet - this is the function of the task
  * Return: Always 0.
  */
 
--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * print_alphabet - this is the function of the task
