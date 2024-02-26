c23126f1c6ce27a85cb5f78159231cb5f4b526bf
jenielr97 <jenielr97@gmail.com>
2024-02-26 13:28:22 -0400

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,24 +1,11 @@
 #include "main.h"
-
 /**
- *_strcat - concatenates  the string pointed to by @src to
- * the end of the string pointed to by @dest
- *@dest: String that will be appended
- *@src: String to be concatenated upon
- *
- * Return: returns poiner to @dest
- */
+*reset_to_98 - reset value of an integer to 98
+*@n: parameter targeted
+*Return : nothing
+*/
 
-char *_strcat(char *dest, char *src)
+void reset_to_98(int *n)
 {
-
-int index = 0, dest_len = 0;
-
-while (dest[index++])
-dest_len++;
-
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
-
-return (dest);
+*n = 98;
 }
