8371c3a99c7a34704b6ef075f09b7b30dce3296d
jenielr97 <jenielr97@gmail.com>
2024-02-27 11:41:02 -0400

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,43 @@
 #include "main.h"
+
 /**
-*  _strncpy - C function that copies a string, including the
-*  terminating null byte, using at most an inputted number of bytes.
-*  If the length of the source string is less than the maximum byte number,
-*  the remainder of the destination string is filled with null bytes.
-*  Works identically to the standard library function `strncpy`.
-*@dest: buffer storing the string copy
-*@src:the source string
-*@n:max nummber of byte copied
-*Return: returns
+* rot13 -  encodes a string using rot13
+* @str:the string targeted
+*Return: returns the encoded string
 */
 
-char *_strncpy(char *dest, char *src, int n)
+char *rot13(char *str)
 {
-int i;
+	int index1, index2;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[i] = src[i];
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+										'G', 'H', 'I', 'J', 'K', 'L',
+										'M', 'N', 'O', 'P', 'Q', 'R',
+										'S', 'T', 'U', 'V', 'W', 'X',
+										'Y', 'Z', 'a', 'b', 'c', 'd',
+										'e', 'f', 'g', 'h', 'i', 'j',
+										'k', 'l', 'm', 'n', 'o', 'p',
+										'q', 'r', 's', 't', 'u', 'v',
+										'w', 'x', 'y', 'z'};
 
-for ( ; i < n; i++)
-dest[i] = '\0';
-
-return (dest);
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+										'T', 'U', 'V', 'W', 'X', 'Y',
+										'Z', 'A', 'B', 'C', 'D', 'E',
+										'F', 'G', 'H', 'I', 'J', 'K',
+										'L', 'M', 'n', 'o', 'p', 'q',
+										'r', 's', 't', 'u', 'v', 'w',
+										'x', 'y', 'z', 'a', 'b', 'c',
+										'd', 'e', 'f', 'g', 'h', 'i',
+										'j', 'k', 'l', 'm'};
+while (str[++index1])
+{
+for (index2 = 0; index2 < 52; index2++)
+{
+if (str[index1] == alphabet[index2])
+{
+str[index1] = rot13key[index2];
+break;
 }
+}
+}
+return (str);
