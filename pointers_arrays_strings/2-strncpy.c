97ca0ec916d098f8f4d9f281c458dd11880c5d74
jenielr97 <jenielr97@gmail.com>
2024-02-28 09:32:28 -0400

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,3 @@
-
-									    
-									    
 #include "main.h"
 
 /**
@@ -22,7 +19,7 @@ int i;
 for (i = 0; i < n && src[i] != '\0'; i++)
 dest[i] = src[i];
 
-for ( ; i < n; i++)
+for (; i < n; i++)
 dest[i] = '\0';
 
 return (dest);
