dd97f1b3ce7f2ea16aab8e0b29347d13d6f93377
jenielr97 <jenielr97@gmail.com>
2024-02-29 22:38:19 -0400

    all

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,9 +3,10 @@
 /**
  * cap_string - function that capitalize first character of a word
  * @str: string to capitalize
- * Return:returns the capitalized string
+ * Return: 'str'
  */
-char *cap_string(char *str)
+
+char cap_string (char *str)
 {
 int index = 0;
 
@@ -14,7 +15,7 @@ while (str[++index])
 while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 
-if (str[index - 1] == ' ' ||
+if(str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
 str[index - 1] == ',' ||
@@ -29,5 +30,6 @@ str[index - 1] == '{' ||
 str[index - 1] == '}')
 str[index] -= 32;
 }
-return (str);
+ return (str);
+
 }
